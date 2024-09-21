0755e0107eb529dfd72a37ab2fd01a1b698fc64b
Steven Nguyen <Steventt1@yahoo.com>
2024-09-21 11:40:17 -0700

    Some Halstead metrics for GPT4o

---
+++ Readability for Code/UnitTestFile.java
@@ -0,0 +1,15 @@
+public class UnitTestFile
+{
+    public static void main (String [] args)
+    {
+        System.out.println("Hi Steven");
+        System.out.println("How are you doing?");
+        printName("Steven");
+    }
+
+
+    public static void printName(String name)
+    {
+        System.out.println(name);
+    }
+}
